68e02c6ef891a4e7b635e0138a4171575d8cd5cb
Lexie <test@gmail.com>
2024-01-12 00:14:12 +0800

    推到github上

---
+++ button.js
@@ -0,0 +1,24 @@
+const ig = document.getElementById("ig");
+
+ig.addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/tatemcrae/?hl=en";
+});
+
+const yt = document.getElementById("yt");
+
+yt.addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com/@TateMcRae";
+});
+
+const twitter = document.getElementById("twitter");
+
+twitter.addEventListener("click", function () {
+  window.location.href =
+    "https://twitter.com/tatemcrae?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor";
+});
+
+const fb = document.getElementById("fb");
+
+fb.addEventListener("click", function () {
+  window.location.href = "https://www.facebook.com/TateMcRaeOfficial/";
+});
